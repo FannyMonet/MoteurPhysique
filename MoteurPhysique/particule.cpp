a409441234a4fe368652d77eadbc51ddac856b4f
Allan <Allan@LAPTOP-VPBHFO0K>
2018-09-12 00:37:45 -0400

    ouiiii

--- MoteurPhysique/particule.cpp
+++ MoteurPhysique/particule.cpp
@@ -15,7 +15,7 @@ void particule::Integrer(float temps)
 	x = x + vitesse.vec[0] * temps;
 	y = y + vitesse.vec[1]* temps;
 	z = z + vitesse.vec[2]* temps;
-	vitesse.vec[0] = vitesse.vec[0] * 0.7 - 9.81*temps;
-	vitesse.vec[1] = vitesse.vec[1] * 0.7 - 9.81*temps;
-	vitesse.vec[2] = vitesse.vec[2] * 0.7 - 9.81*temps;
+	vitesse.vec[0] = vitesse.vec[0] * 0.7;// -9.81*temps;
+	vitesse.vec[1] = vitesse.vec[1] * 0.7;// -9.81*temps;
+	vitesse.vec[2] = vitesse.vec[2]*0.7 - 9.81*temps;
 };
