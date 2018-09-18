5f1376d0e61616fb67d07f865eb8961b6486d509
FannyMonet <f4f4y0p@gmail.com>
2018-09-18 17:32:34 -0400

    ajout du dumping et de la masse à la classe particule

--- MoteurPhysique/particule.h
+++ MoteurPhysique/particule.h
@@ -8,26 +8,33 @@ using namespace std;
 class particule
 {
 public:
-	
 	vecteur position = vecteur();
 	vecteur vitesse = vecteur();
 	int numero;
+	float dumping;
+	float masse;
 
-	particule(float posx, float posy, float posz, float vitx, float vity, float vitz, int n)
+	particule(float posx, float posy, float posz, float vitx, float vity, float vitz, int n, int d, float m)
 	{
 		numero = n;
+		dumping = d;
+		masse = m;
 		position = vecteur(posx, posy, posz);
 		vitesse = vecteur(vitx, vity, vitz);
 	};
 	particule()
 	{
-		numero = -1;
+		numero = 0;
+		dumping = 0;
+		masse = 0;
 		position = vecteur(0, 0, 0);
 		vitesse = vecteur(0, 0, 0);
 	};
 	particule(const particule& particule)
 	{
 		numero = particule.numero;
+		dumping = particule.dumping;
+		masse = particule.masse;
 		position = vecteur(particule.position.x, particule.position.y, particule.position.z);
 		vitesse = vecteur(particule.vitesse.x, particule.vitesse.y, particule.vitesse.z);
 	}
