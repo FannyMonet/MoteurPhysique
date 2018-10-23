a9c7d389600039322aa9021027f590fdbdffa575
FannyMonet <f4f4y0p@gmail.com>
2018-10-23 14:53:51 -0400

    fix moteurphysique.cpp et debug des classes particuleSpring et ParticuleAnchoredSpring

--- MoteurPhysique/ParticuleAnchoredSpring.cpp
+++ MoteurPhysique/ParticuleAnchoredSpring.cpp
@@ -14,12 +14,14 @@ ParticuleAnchoredSpring::~ParticuleAnchoredSpring()
 void ParticuleAnchoredSpring::updateforceparticule(particule particule, float duration)
 {
 	//int force;
-
-	double deltax = particule.position.x - pointancrage.x;
-	double deltay = particule.position.y - pointancrage.y;
-	double deltaz = particule.position.z - pointancrage.z;
+	
+	double deltax = particule.dimension == 3 ? particule.position3D.x - pointancrage3D.x : particule.position2D.x - pointancrage2D.x;
+	double deltay = particule.dimension == 3 ? particule.position3D.y - pointancrage3D.y : particule.position2D.y - pointancrage2D.y;
+	double deltaz;
+	if(particule.dimension == 3)
+		deltaz = particule.position3D.z - pointancrage3D.z;
 	//double vectunitaired = sqrt(x*x+y*y+z*z);
 
-	const vecteur &force = vecteur(-k*(sqrt(deltax*deltax) - L0), -k*(sqrt(deltay*deltay) - L0), -k*(sqrt(deltaz*deltaz) - L0));
-	particule.addForce(force);
+	//const vecteur &force = vecteur(-k*(sqrt(deltax*deltax) - L0), -k*(sqrt(deltay*deltay) - L0), -k*(sqrt(deltaz*deltaz) - L0));
+	//particule.addForce(force);
 }
--- MoteurPhysique/ParticuleSpring.cpp
+++ MoteurPhysique/ParticuleSpring.cpp
@@ -16,12 +16,14 @@ ParticuleSpring::~ParticuleSpring()
 void ParticuleSpring::updateforceparticule(particule  particule, float duration)
 {
 	//int force;
-
-	double deltax = particule.position.x - autreparticule.position.x;
-	double deltay = particule.position.y - autreparticule.position.y;
-	double deltaz = particule.position.z - autreparticule.position.z;
+	int dim = particule.dimension;
+	double deltax = dim == 3 ? particule.position3D.x - autreparticule.position3D.x : particule.position2D.x - autreparticule.position2D.x;
+	double deltay = dim == 3 ?  particule.position3D.y - autreparticule.position3D.y : particule.position2D.y - autreparticule.position2D.y;
+	double deltaz;
+	if(dim == 3)
+		 deltaz = particule.position3D.z - autreparticule.position3D.z;
 	//double vectunitaired = sqrt(x*x+y*y+z*z);
 	
-	const vecteur &force= vecteur(-k*(sqrt(deltax*deltax)-L0), -k*(sqrt(deltay*deltay) - L0), -k*(sqrt(deltaz*deltaz) - L0));
-	particule.addForce(force);
+	//const vecteur &force= vecteur(-k*(sqrt(deltax*deltax)-L0), -k*(sqrt(deltay*deltay) - L0), -k*(sqrt(deltaz*deltaz) - L0));
+	//particule.addForce(force);
 }
