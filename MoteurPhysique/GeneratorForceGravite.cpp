e7b5dc61e81f115b311bd84c48f9c8ba46fd29f3
Allan <Allan@LAPTOP-VPBHFO0K>
2018-10-24 11:01:51 -0400

    test

--- MoteurPhysique/GeneratorForceGravite.cpp
+++ MoteurPhysique/GeneratorForceGravite.cpp
@@ -10,8 +10,18 @@ GeneratorForceGravite::GeneratorForceGravite()
 
 void GeneratorForceGravite::updateforceparticule(particule particule, float duration)
 {
+	/*int dim = particule.dimension;
+	if (dim == 2)
+	{
+		const  vecteur2D forceCst(-k*(sqrt(deltax*deltax) - L0), -k*(sqrt(deltay*deltay) - L0));
+		particule.addForce(forceCst);
+	}
+	if (dim == 3)
+	{
+		deltaz = particule.position3D.z - pointancrage3D.z;
+		const  vecteur3D forceCst(-k*(sqrt(deltax*deltax) - L0), -k*(sqrt(deltay*deltay) - L0), -k*(sqrt(deltaz*deltaz) - L0));
+		particule.addForce(forceCst);
+	}*/
 }
 
-void updateforceparticule(particule particule, float duration)
-{
-}
+
