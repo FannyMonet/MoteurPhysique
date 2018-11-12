4b2b333d070dd17dc11148340282520782312f7e
FannyMonet <f4f4y0p@gmail.com>
2018-11-12 00:31:35 -0500

    ajout des fichiers dans le projet

--- MoteurPhysique/vecteur4D.cpp
+++ MoteurPhysique/vecteur4D.cpp
@@ -6,10 +6,10 @@ vecteur4D::vecteur4D()
 {
 }
 
-vecteur4D::vecteur4D (double cw, double cx, double cy, double cz)
+vecteur4D::vecteur4D (double _w, double _x, double _y, double _z)
 {
-	w = cw;
-	x = cx;
-	y = cy;
-	z = cz;
+	w = _w;
+	x = _x;
+	y = _y;
+	z = _z;
 }
