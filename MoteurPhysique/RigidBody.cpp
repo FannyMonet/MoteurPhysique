12dbd5f57c1ea6787db07f1e796b9de8066de05b
FannyMonet <f4f4y0p@gmail.com>
2018-11-20 14:51:37 -0500

    création des premières méthodes de RigidBody

--- MoteurPhysique/RigidBody.cpp
+++ MoteurPhysique/RigidBody.cpp
@@ -5,3 +5,33 @@
 RigidBody::RigidBody()
 {
 }
+
+Matrice3 RigidBody::calculDonneesDerivees()
+{
+	// Calcul transformMatrice
+}
+void RigidBody::addForceAtPoint(vecteur3D force, vecteur3D point)
+{
+	//Convertir point en coordonnées relatives au centre de masse
+	// ajouter force à forceAccum
+	// ajouter point x Force à TorqueAccum
+}
+
+void RigidBody::addForceAtBodyPoint(vecteur3D force, vecteur3D point)
+{
+	//Convertir point vers le repère du monde
+	addForceAtPoint(force, point);
+}
+
+void RigidBody::Integrer(float duree)
+{
+	//Calculer l'accélération linéaire à partir de ForceAccum
+	//Calculer l'accélération angulaire à partir de TorqueAccum
+	//Update velocité linéaire
+	//Update velocité angulaire
+	//Ajouter le drag aux deux vélocités
+	//Update position
+	//Update orientation
+	//Calculer les "derived data" // Transform matrix et I-1
+	//Clear accumulator
+}
--- MoteurPhysique/RigidBody.h
+++ MoteurPhysique/RigidBody.h
@@ -6,13 +6,20 @@ class RigidBody
 {
 public:
 	float inverseMass;
-	float linearDampling;
+	float linearDamping;
+	float angularDamping;
 	vecteur3D position;
 	vecteur3D velocity;
 	Quaternion orientation;
 	vecteur3D rotation;
 	Matrice3 transformMatrice;
+	Matrice3 inverseInertieTensor;
 
 	RigidBody();
+
+	Matrice3 calculDonneesDerivees();
+	void addForceAtPoint(vecteur3D force, vecteur3D point);
+	void addForceAtBodyPoint(vecteur3D force, vecteur3D point);
+	void Integrer(float duree);
 };
 
