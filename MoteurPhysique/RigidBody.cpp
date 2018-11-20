8a1f6b684ac9bfd5ca8229eb2717507fadb597a6
FannyMonet <f4f4y0p@gmail.com>
2018-11-20 18:56:55 -0500

    ajout des conversions quaternions en angles d'euler

--- MoteurPhysique/RigidBody.cpp
+++ MoteurPhysique/RigidBody.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "RigidBody.h"
 
+# define M_PI           3.14159265358979323846  /* pi */
 
 RigidBody::RigidBody(vecteur3D _position, vecteur3D _velocity, vecteur3D _orientation)
 {
@@ -10,6 +11,7 @@ RigidBody::RigidBody(vecteur3D _position, vecteur3D _velocity, vecteur3D _orient
 	transformMatrice = Matrice3(position.x, 0, 0,
 								0, position.y, 0,
 								0, 0, position.z);
+	orientation = toQuaternion(_orientation);
 }
 
 Matrice3 RigidBody::calculDonneesDerivees()
@@ -66,4 +68,48 @@ void RigidBody::clearAccumulators()
 {
 	forceAccum = vecteur3D(0, 0, 0);
 	torqueAccum = vecteur3D(0, 0, 0);
+}
+//source: wikipedia https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
+static vecteur3D toEulerAngle(const Quaternion& q) 
+{
+	double roll;
+	double pitch;
+	double yaw;
+	// roll (x-axis rotation)
+	double sinr_cosp = +2.0 * (q.w * q.x + q.y * q.z);
+	double cosr_cosp = +1.0 - 2.0 * (q.x * q.x + q.y * q.y);
+	roll = atan2(sinr_cosp, cosr_cosp);
+
+	// pitch (y-axis rotation)
+	double sinp = +2.0 * (q.w * q.y - q.z * q.x);
+	if (fabs(sinp) >= 1)
+		pitch = copysign(M_PI / 2, sinp); // use 90 degrees if out of range
+	else
+		pitch = asin(sinp);
+
+	// yaw (z-axis rotation)
+	double siny_cosp = +2.0 * (q.w * q.z + q.x * q.y);
+	double cosy_cosp = +1.0 - 2.0 * (q.y * q.y + q.z * q.z);
+	yaw = atan2(siny_cosp, cosy_cosp);
+	return vecteur3D(roll, pitch, yaw);
+}
+static Quaternion toQuaternion(vecteur3D orientation)
+{
+	double roll = orientation.x;
+	double pitch = orientation.y;
+	double yaw = orientation.z;
+	// Abbreviations for the various angular functions
+	double cy = cos(yaw * 0.5);
+	double sy = sin(yaw * 0.5);
+	double cr = cos(roll * 0.5);
+	double sr = sin(roll * 0.5);
+	double cp = cos(pitch * 0.5);
+	double sp = sin(pitch * 0.5);
+
+	Quaternion q;
+	q.w = cy * cr * cp + sy * sr * sp;
+	q.x = cy * sr * cp - sy * cr * sp;
+	q.y = cy * cr * sp + sy * sr * cp;
+	q.z = sy * cr * cp - cy * sr * sp;
+	return q;
 }
