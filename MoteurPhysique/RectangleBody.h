6b848e7e22d5e39d7295db8f02fcfb3852c950f6
FannyMonet <f4f4y0p@gmail.com>
2018-11-21 11:26:20 -0500

    fix constructeur RectangleBody

--- MoteurPhysique/RectangleBody.h
+++ MoteurPhysique/RectangleBody.h
@@ -6,7 +6,6 @@ class RectangleBody : RigidBody
 {
 public:
 	vecteur3D dimensions;
-	vecteur3D position;
 
 	RectangleBody(vecteur3D position, vecteur3D velocity, vecteur3D orientation, vecteur3D rotation, float linearDamping, float angularDamping, vecteur3D dim);
 	virtual Matrice3 getInertieTensor();
